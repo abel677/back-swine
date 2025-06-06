32934b6ce633b9af81c7a106c314e938f7e09fc5
abel677 <abelandrade677@gmail.com>
2025-06-06 01:02:28 -0500

    copy views with copyFiles

--- src/core/shared/utils/Validators.ts
+++ src/core/shared/utils/Validators.ts
@@ -4,7 +4,7 @@ export class Validators {
   }
 
   static get password() {
-    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
   }
 
   static get date() {
